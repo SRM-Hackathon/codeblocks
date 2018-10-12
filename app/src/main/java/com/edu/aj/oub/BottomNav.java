98c6a4487af152fba8ce6a670b2b502465cf85f2
ajkiran28 <kiran.ajay28@gmail.com>
2018-10-12 17:59:58 +0530

    Merged Map

--- app/src/main/java/com/edu/aj/oub/BottomNav.java
+++ app/src/main/java/com/edu/aj/oub/BottomNav.java
@@ -3,6 +3,7 @@ package com.edu.aj.oub;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
@@ -17,13 +18,16 @@ public class BottomNav extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText("Parking");
+                case R.id.nav_park:
+                    MapPark mapPark= new MapPark();
+                    FragmentManager fragmentManager= getSupportFragmentManager();
+                    fragmentManager.beginTransaction().replace(R.id.map, mapPark).commit();
+
                     return true;
-                case R.id.navigation_dashboard:
+                case R.id.nav_bike:
                     mTextMessage.setText("Bike Taxi");
                     return true;
-                case R.id.navigation_notifications:
+                case R.id.nav_cab:
                     mTextMessage.setText("Cab");
                     return true;
             }
